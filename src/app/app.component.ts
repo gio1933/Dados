0df5f1518175dae8dc79ca45e462363ab3aff7ea
gio1933 <gio_1933@hotmail.com>
2022-08-08 13:52:54 -0500

    Variables
    Initialize the variables of the dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dados';
+  dadoIzquierda = '../assets/img/dice1.png';
+  dadoDerecha = '../assets/img/dice4.png';
+  numero1!: number;
+  numero2!: number;
+
+  tirarDados(): void{
+    this.numero1 = Math.random();
+    this.numero2 = Math.random();
+    console.log(this.numero1);
+    console.log(this.numero2);
+  }
 }
